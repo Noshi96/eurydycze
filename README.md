3b6c5339430c6984f4d37e901a5efe865a1dc45f
Noshi96 <2spawel2@gmail.com>
2023-06-27 15:24:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# eurydycze
---
+++ src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import "./styles.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.createElement("div");
+  // container.classList.add("new-class");
+  // container.innerHTML = "siema";
+  // document.querySelector(".main-class").append(container);
+  // document.querySelector(".header-class").append(container);
+});
