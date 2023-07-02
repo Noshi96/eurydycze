c01b64ed2aa0c7f9237de7a38fc3c255c3fb6e7a
Noshi96 <2spawel2@gmail.com>
2023-07-02 22:02:08 +0200

    feat: change png to webp

--- src/index.js
+++ src/index.js
@@ -11,9 +11,38 @@ import "./contact.styles.css";
 import "./price-list.styles.css";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const container = document.createElement("div");
-  // container.classList.add("new-class");
-  // container.innerHTML = "siema";
-  // document.querySelector(".main-class").append(container);
-  // document.querySelector(".header-class").append(container);
+  // Smooth scroll function
+  function smoothScroll(target, duration) {
+    const targetElement = document.querySelector(target);
+    const targetPosition = targetElement.offsetTop;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+
+    function scrollAnimation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(scrollAnimation);
+    }
+
+    // Easing function
+    function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(scrollAnimation);
+  }
+
+  // Smooth scroll event listener using event delegation
+  document.querySelector("nav").addEventListener("click", function (event) {
+    if (event.target.tagName === "A") {
+      const target = event.target.getAttribute("href");
+      smoothScroll(target, 1000); // Adjust the duration as per your preference
+    }
+  });
 });
